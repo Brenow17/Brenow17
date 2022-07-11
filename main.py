5d4df86f24c2c9233424e1a79a3e38ed469446b9
Brenow <brenowsuper50@gmail.com>
2022-07-11 01:49:48 -0300

    Versão 1.0.0

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Digite algum numero:")
+n1 = int(input())
+print("Digite outro numero:")
+n2 = int(input())
+print("Digite outro numero:")
+n3 = int(input())
+soma = n1 + n2 + n3
+print(f'\n\n A soma entre eles eh {soma}')
